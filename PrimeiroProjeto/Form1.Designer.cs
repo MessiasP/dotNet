0ccebb5cf43ce96d85140643be1d6ce20be77183
messias.souza <messias.souza@basis.com.br>
2018-09-04 13:15:26 -0300

    Primeiro projeto

---
+++ PrimeiroProjeto/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace PrimeiroProjeto
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtUsuario = new System.Windows.Forms.TextBox();
+            this.LblUsuario = new System.Windows.Forms.Label();
+            this.TxtSenha = new System.Windows.Forms.TextBox();
+            this.BtnRecuperar = new System.Windows.Forms.Button();
+            this.BtnLogar = new System.Windows.Forms.Button();
+            this.LblSenha = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TxtUsuario
+            // 
+            this.TxtUsuario.Location = new System.Drawing.Point(290, 130);
+            this.TxtUsuario.MaxLength = 300;
+            this.TxtUsuario.Name = "TxtUsuario";
+            this.TxtUsuario.Size = new System.Drawing.Size(228, 20);
+            this.TxtUsuario.TabIndex = 1;
+            this.TxtUsuario.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // LblUsuario
+            // 
+            this.LblUsuario.AutoSize = true;
+            this.LblUsuario.BackColor = System.Drawing.Color.Transparent;
+            this.LblUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblUsuario.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.LblUsuario.Location = new System.Drawing.Point(299, 103);
+            this.LblUsuario.Name = "LblUsuario";
+            this.LblUsuario.Size = new System.Drawing.Size(81, 24);
+            this.LblUsuario.TabIndex = 5;
+            this.LblUsuario.Text = "Usuario";
+            this.LblUsuario.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // TxtSenha
+            // 
+            this.TxtSenha.Location = new System.Drawing.Point(290, 180);
+            this.TxtSenha.MaxLength = 16;
+            this.TxtSenha.Name = "TxtSenha";
+            this.TxtSenha.PasswordChar = '*';
+            this.TxtSenha.Size = new System.Drawing.Size(228, 20);
+            this.TxtSenha.TabIndex = 2;
+            // 
+            // BtnRecuperar
+            // 
+            this.BtnRecuperar.BackColor = System.Drawing.Color.Red;
+            this.BtnRecuperar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnRecuperar.ForeColor = System.Drawing.SystemColors.Info;
+            this.BtnRecuperar.Location = new System.Drawing.Point(303, 267);
+            this.BtnRecuperar.Margin = new System.Windows.Forms.Padding(1);
+            this.BtnRecuperar.Name = "BtnRecuperar";
+            this.BtnRecuperar.Size = new System.Drawing.Size(196, 30);
+            this.BtnRecuperar.TabIndex = 4;
+            this.BtnRecuperar.Text = "RECUPERAR SENHA";
+            this.BtnRecuperar.UseVisualStyleBackColor = false;
+            // 
+            // BtnLogar
+            // 
+            this.BtnLogar.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.BtnLogar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnLogar.ForeColor = System.Drawing.SystemColors.Info;
+            this.BtnLogar.Location = new System.Drawing.Point(303, 231);
+            this.BtnLogar.Margin = new System.Windows.Forms.Padding(1);
+            this.BtnLogar.Name = "BtnLogar";
+            this.BtnLogar.Size = new System.Drawing.Size(196, 30);
+            this.BtnLogar.TabIndex = 3;
+            this.BtnLogar.Text = "LOGAR";
+            this.BtnLogar.UseVisualStyleBackColor = false;
+            this.BtnLogar.Click += new System.EventHandler(this.btnLogar_Click);
+            // 
+            // LblSenha
+            // 
+            this.LblSenha.AutoSize = true;
+            this.LblSenha.BackColor = System.Drawing.Color.Transparent;
+            this.LblSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblSenha.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.LblSenha.Location = new System.Drawing.Point(299, 153);
+            this.LblSenha.Name = "LblSenha";
+            this.LblSenha.Size = new System.Drawing.Size(70, 24);
+            this.LblSenha.TabIndex = 6;
+            this.LblSenha.Text = "Senha";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::PrimeiroProjeto.Properties.Resources.test;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnLogar);
+            this.Controls.Add(this.LblSenha);
+            this.Controls.Add(this.BtnRecuperar);
+            this.Controls.Add(this.TxtSenha);
+            this.Controls.Add(this.LblUsuario);
+            this.Controls.Add(this.TxtUsuario);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Login";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FormFundo_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtUsuario;
+        private System.Windows.Forms.Label LblUsuario;
+        private System.Windows.Forms.TextBox TxtSenha;
+        private System.Windows.Forms.Button BtnRecuperar;
+        private System.Windows.Forms.Button BtnLogar;
+        private System.Windows.Forms.Label LblSenha;
+    }
+}
+
---
+++ PrimeiroProjeto/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrimeiroProjeto
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormFundo_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogar_Click(object sender, EventArgs e)
+        {
+            if ( TxtSenha.Text == "" || TxtUsuario.Text == "" )
+            {
+                MessageBox.Show("Campo Invalido", "ATENÇÃO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                TxtUsuario.Focus();
+            }
+        }
+    }
+}
